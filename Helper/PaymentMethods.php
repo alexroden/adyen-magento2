d244f4a7e0a08ed96bf4b186c067b7606e3e5a4d
Alex Roden <alex.broom-roden@oneiota.co.uk>
2021-10-20 14:06:21 +0100

    feat: support dynamic channel for retrieving payment methods

--- Helper/PaymentMethods.php
+++ Helper/PaymentMethods.php
@@ -138,12 +138,13 @@ class PaymentMethods extends AbstractHelper
     /**
      * @param $quoteId
      * @param null $country
+     * @param $channel
      * @return array
      * @throws \Adyen\AdyenException
      * @throws \Magento\Framework\Exception\LocalizedException
      * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
-    public function getPaymentMethods($quoteId, $country = null)
+    public function getPaymentMethods($quoteId, $country = null, $channel = null)
     {
         // get quote from quoteId
         $quote = $this->quoteRepository->getActive($quoteId);
@@ -155,16 +156,17 @@ class PaymentMethods extends AbstractHelper
 
         $this->setQuote($quote);
 
-        return $this->fetchPaymentMethods($country);
+        return $this->fetchPaymentMethods($country, $channel);
     }
 
     /**
      * @param $country
+     * @param $channel
      * @return array
      * @throws \Adyen\AdyenException
      * @throws \Magento\Framework\Exception\LocalizedException
      */
-    protected function fetchPaymentMethods($country)
+    protected function fetchPaymentMethods($country, $channel = null)
     {
         $quote = $this->getQuote();
         $store = $quote->getStore();
@@ -175,7 +177,7 @@ class PaymentMethods extends AbstractHelper
             return json_encode([]);
         }
 
-        $paymentMethodRequest = $this->getPaymentMethodsRequest($merchantAccount, $store, $country, $quote);
+        $paymentMethodRequest = $this->getPaymentMethodsRequest($merchantAccount, $store, $country, $quote, $channel);
         $responseData = $this->getPaymentMethodsResponse($paymentMethodRequest, $store);
 
         if (empty($responseData['paymentMethods'])) {
@@ -320,6 +322,7 @@ class PaymentMethods extends AbstractHelper
      * @param \Magento\Store\Model\Store $store
      * @param $country
      * @param \Magento\Quote\Model\Quote $quote
+     * @param $channel
      * @return array
      * @throws \Exception
      */
@@ -327,12 +330,13 @@ class PaymentMethods extends AbstractHelper
         $merchantAccount,
         \Magento\Store\Model\Store $store,
         $country,
-        \Magento\Quote\Model\Quote $quote
+        \Magento\Quote\Model\Quote $quote,
+        $channel = 'Web'
     ) {
         $currencyCode = $this->chargedCurrency->getQuoteAmountCurrency($quote)->getCurrencyCode();
 
         $paymentMethodRequest = [
-            "channel" => "Web",
+            "channel" => $channel,
             "merchantAccount" => $merchantAccount,
             "countryCode" => $this->getCurrentCountryCode($store, $country),
             "shopperLocale" => $this->adyenHelper->getCurrentLocaleCode($store->getId()),
